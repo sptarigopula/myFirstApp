eee2b335bd5c3754e6330d6bb429651f6f9a03c9
sripushpa_tarigopula <sripushpa.tarigopula@invitae.com>
2020-07-05 15:45:18 -0700

    MYAPP-1: Initial Commit

---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're in mysite project.")
+
+def welcome_user(request, name: str):
+    return HttpResponse(f"Hello, {name}. You're in mysite project.")
+
+def calculate_sum(request, first: int, second: int):
+    return HttpResponse(first+second)
